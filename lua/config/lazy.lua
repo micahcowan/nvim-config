2c3a3805758560fdb99f11b651b9614edaf4a16a
Micah Cowan <micah@addictivecode.org>
2026-01-26 03:37:47 -0800

    "stable" lazy.nvim breaks on first nvim run (only)

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -3,7 +3,9 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({
-      "git", "clone", "--filter=blob:none", "--branch=stable",
+      "git", "clone", "--filter=blob:none",
+      -- "--branch=stable",     <-- this breaks first nvim invocation,
+      --                            as of 2026-01-26
       "--depth", "1", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
