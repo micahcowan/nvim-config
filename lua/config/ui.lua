9bb5e6a5f193ed427e82ea51cd03ec013a9a4360
Micah Cowan <micah@addictivecode.org>
2026-01-28 02:08:34 -0800

    Work around Neovide's guifont syntax inflexibility

--- lua/config/ui.lua
+++ lua/config/ui.lua
@@ -1,5 +1,11 @@
---vim.o.guifont = "FiraCode Nerd Font Mono,Ubuntu Mono:h9"
-vim.o.guifont = "Ubuntu Mono:h10,Monaco:h10"
+-- guifont is supposed to allow multiple fonts.
+-- Neovide doesn't handle that well.
+--   vim.fn.has('mac') returns false positive at init time.
+if vim.loop.os_uname().sysname == 'Linux' then
+    vim.o.guifont = "Ubuntu Mono:h10"
+else
+    vim.o.guifont = "Monaco:h10"
+end
 vim.g.neovide_remember_window_size = true
 
 ---- Following doesn't work, bc the default TUI does UIEnter.
