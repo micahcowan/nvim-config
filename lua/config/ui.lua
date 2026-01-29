6a06cbb2e81efcaba977ef0f1827136b702ae635
Micah Cowan <mcowan@akamai.com>
2026-01-29 01:57:13 -0500

    Add tabby plugin

--- lua/config/ui.lua
+++ lua/config/ui.lua
@@ -1,20 +1,13 @@
 -- guifont is supposed to allow multiple fonts.
 -- Neovide doesn't handle that well.
---   vim.fn.has('mac') returns false positive at init time.
-if vim.loop.os_uname().sysname == 'Linux' then
-    vim.o.guifont = "Ubuntu Mono:h10"
-else
-    vim.o.guifont = "Monaco:h10"
-end
-vim.g.neovide_remember_window_size = true
+--   vim.fn.has('mac') returns false positive at init time,
+-- so can't use that to check if we're mac
 
----- Following doesn't work, bc the default TUI does UIEnter.
----- Would need to differentiate UIs or something.
---
--- vim.api.nvim_create_autocmd('UIEnter', {
---     callback = function()
---         
--- 	   vim.o.lines = 46
--- 	   vim.o.columns = 161
---     end
--- })
+-----  DISABLED: do this in config.local instead!
+-----  if vim.loop.os_uname().sysname == 'Linux' then
+-----      vim.o.guifont = "Ubuntu Mono:h10"
+-----  else
+-----      vim.o.guifont = "Monaco:h10"
+-----  end
+
+vim.g.neovide_remember_window_size = true
