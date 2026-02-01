c46354b0b0922ae2609163c77d17be02658dcb43
Micah Cowan <micah@addictivecode.org>
2026-02-01 14:56:09 -0800

    <C-W><C-Q>: automatically kill any dangling terms

---
+++ lua/mjc-util.lua
@@ -0,0 +1,32 @@
+local M = {}
+
+function M.filter_term_wins(wins)
+    local terms = {}
+    for _, wnr in ipairs(wins) do
+        local bnr = vim.api.nvim_win_get_buf(wnr)
+        local ty = vim.api.nvim_get_option_value(
+            'buftype',
+            { buf = bnr })
+        if ty == 'terminal' then
+            table.insert(terms, wnr)
+        end
+    end
+
+    if #terms == 0 then
+        return nil
+    else
+        return terms
+    end
+end
+
+function M.get_terms_in_tab(tab_nr)
+    local wins = vim.api.nvim_tabpage_list_wins(tab_nr)
+    return M.filter_term_wins(wins)
+end
+
+function M.get_terms_in_curtab()
+    local tnr = vim.api.nvim_get_current_tabpage()
+    return M.get_terms_in_tab(tnr)
+end
+
+return M
