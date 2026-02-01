4a4c5d6c699dd6fa15cce1cdacdfef6703b6311c
Micah Cowan <micah@addictivecode.org>
2026-01-31 17:19:45 -0800

    Split misc keymap and autocmd to their own configs

---
+++ lua/config/autocmd.lua
@@ -0,0 +1,37 @@
+-- config.autocmd: MISC AUTOCMD CHANGES
+
+vim.api.nvim_create_augroup('mcowan-init', {})
+
+-- Bindings to jump forward/back by hyperlink
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'help',
+    callback = function(opts)
+        vim.wo.relativenumber = true
+
+        vim.keymap.set('n', '<Tab>', '', {
+            callback = function()
+                vim.fn.search('\\([|\'`]\\)[^ ]*\\1')
+            end,
+            noremap = true,
+        })
+        vim.keymap.set('n', '<S-Tab>', '', {
+            callback = function()
+                vim.fn.search('\\([|\'`]\\)[^ ]*\\1', 'b')
+            end,
+            noremap = true,
+        })
+    end,
+    group = 'mcowan-init'
+})
+
+-- help/man keybindings to be more like "less" (the pager)
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = {'help', 'man'},
+    callback = function()
+        vim.keymap.set('n', '<Space>', '<C-F>', { noremap = true })
+        vim.keymap.set('n', 'b', '<C-B>', { noremap = true })
+        -- Man already has this, but help doesn't:
+        vim.keymap.set('n', 'q', ':q<CR>', { noremap = true })
+    end,
+    group = 'mcowan-init',
+})
